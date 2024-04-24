eaf3dd649f710cd2dc8df3ed7d2c4d19a86f4d2c
Charles Knepshield <aknepshield@gmail.com>
2024-04-23 19:29:31 -0500

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./stowawayapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations stowawayapi
+python3 manage.py migrate stowawayapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
---
+++ stowawayapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StowawayapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stowawayapi'
